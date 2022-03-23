f7446004b33c97e674c1d8967b04b91bda145bac
Mervyn59484563 <102244872+Mervyn59484563@users.noreply.github.com>
2022-03-23 19:42:34 +0300

    Create mervyn.js

---
+++ mervyn.js
@@ -0,0 +1,7 @@
+<script src="/calendar_kdg_utf8.min.js" type="text/javascript"></script>
+<form action="">
+с <input type="text" value="dd-mm-yy" onfocus="this.select();_Calendar.lcs(this)"
+    onclick="event.cancelBubble=true;this.select();_Calendar.lcs(this)"  data-yearfrom="-80" data-yearto="5">
+по <input type="text" value="dd-mm-yy" onfocus="this.select();_Calendar.lcs(this)"
+    onclick="event.cancelBubble=true;this.select();_Calendar.lcs(this)" data-yearto="5">
+</form>
